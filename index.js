c238cc24b9c7251c63813bdf637b521911dd3be3
arielmerin <arielmerino@ciencias.unam.mx>
2021-04-20 18:22:39 -0500

    Primer intento para pintar las preguntas en la pantalla

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://opentdb.com/api.php?amount=10")
+fetch("https://opentdb.com/api.php?amount=10&type=multiple")
     .then(response => response.json())
     .then(data => printQuestions(data.results))
 
@@ -6,24 +6,31 @@ fetch("https://opentdb.com/api_category.php")
     .then(response => response.json())
     .then(datos => printCategories(datos.trivia_categories))
 
-fetch("https://opentdb.com/api.php?amount=1")
-    .then(respuesta => console.log(respuesta.json()))
 
-fetch("https://opentdb.com/api.php?amount=1")
-    .then(respuesta => respuesta.json())
-    .then(datos => console.log(datos))
-
-
-
-
-function printQuestions(questions) {
+function printQuestions(resultados) {
     const container = document.getElementById('questions-container')
-    questions.forEach((question) => {
+    let i = 1;
+    resultados.forEach((resultado) => {
         container.innerHTML += `
                     <div class="col-4 mt-3">
                 <div class="card h-100">
                     <div class="card-body">
-                        ${question.question}
+                        <h5 class="display-5 text-center">
+                        Pregúnta # ${i++}
+                        </h5>
+                        <p class="text-center">${resultado.question}</p>
+                        
+                            <div class="row mx-auto justify-content-center">
+                                <button type="button" class="btn btn-outline-primary ">
+                                    ${resultado.incorrect_answers[0]}
+                                </button>                                     
+                                <button type="button" class="btn btn-outline-primary ">
+                                    ${resultado.incorrect_answers[1]}
+                                </button>                                         
+                                <button type="button" class="btn btn-outline-primary ">
+                                    ${resultado.incorrect_answers[2]}
+                                </button>       
+                            </div>
                     </div>
                 </div>
             </div>
